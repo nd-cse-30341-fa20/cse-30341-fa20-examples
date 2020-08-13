679e534e6a6f10e8c0a514d9f74332dfaf38c239
Peter Bui <pbui@nd.edu>
2020-08-13 09:10:00 -0400

    Add lecture04 example (hello-n)

---
+++ lecture04/hello-n.c
@@ -0,0 +1,46 @@
+/* hello-n.c */
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+    	fprintf(stderr, "Usage: %s N\n", argv[0]);
+    	return EXIT_FAILURE;
+    }
+
+    int n = strtol(argv[1], NULL, 10);
+
+    for (int i = 0; i < n; i++) {
+    	pid_t pid = fork();
+    	if (pid < 0) {	// Error
+	    fprintf(stderr, "fork failed: %s\n", strerror(errno));
+    	    return EXIT_FAILURE;
+	}
+
+    	if (pid == 0) {	// Child
+    	    sleep(i);
+    	    printf("Hello from child %d\n", i);
+    	    _exit(0);
+	} /*else {	// Version 2: Parent waits after each child (serial)
+	    while (wait(NULL) < 0);
+	}
+	*/
+
+    }
+
+    // Version 1: No wait at all... children keep running when parent exits
+
+    // Version 3: Wait after launching all children (parallel)
+    for (int i = 0; i < n; i++) {
+	while (wait(NULL) < 0);
+    }
+
+    return EXIT_SUCCESS;
+}
